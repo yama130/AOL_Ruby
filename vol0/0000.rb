1dea967ca0ea65681180cba76043f9649293ed92
yama130 <ouos@hotmail.com>
2015-08-08 13:26:49 +0900

    配列の作り方

---
+++ vol0/0000.rb
@@ -0,0 +1,5 @@
+for i in 1...10
+  for j in 1...10
+    printf("%dx%d=%d\n",i,j,i*j)
+  end
+end
---
+++ vol0/0001.rb
@@ -0,0 +1,8 @@
+a=[]
+for i in 1..10
+  a.push(gets.to_i)
+end
+a.sort!
+puts a[9]
+puts a[8]
+puts a[7]
